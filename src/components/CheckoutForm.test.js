894503c793f5937079818decbdd385577f1d1ba4
joseguevara29 <guevarabus29@gmail.com>
2021-06-13 22:34:49 -0400

    Finished MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -4,6 +4,20 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.getByText(/Checkout Form/i);
 
-test("form shows success message on submit with form details", () => {});
+  expect(header).toBeVisible();
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+});
+
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  const buttonCheckOut = screen.getByRole("button");
+  userEvent.click(buttonCheckOut);
+  const success = screen.getByTestId("successMessage");
+
+  expect(success).toBeInTheDocument();
+});
